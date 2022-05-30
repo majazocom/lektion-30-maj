0942e02bb7c381f5577d4b23a06b686f340390b0
Maja Gedda <maja.gedda@zocom.se>
2022-05-30 13:31:53 +0200

    something

--- plant-app/app.js
+++ plant-app/app.js
@@ -11,9 +11,12 @@ let plantList = [
 const plantListElement = document.getElementById('plant-list');
 
 function generatePlantListUI() {
+    //först behöver vi tömma vår ul på all eventuellt innehåll
+    //för att inte skriva in samma plantor flera gånger
+    plantListElement.innerHTML = "";
+
     //loopa över listan och för varje planta skall vi
     //trycka in den i vår ul med id plant-list
-
     for (let i = 0; i < plantList.length; i++) {
         //sparar nuvarande element i loopen i variabeln 'plant'
         let plant = plantList[i];
@@ -21,10 +24,24 @@ function generatePlantListUI() {
         let newElement = document.createElement('li');
         //lägger in strängen som vi fått från listan genom vår loop i vårt nya element
         newElement.innerText = plant;
-        console.log(newElement);
         //nu vill vi lägga in plantan i vår ul
         plantListElement.appendChild(newElement);
     }
 };
-
 generatePlantListUI();
+
+//1. lägga till en eventlyssnare på vår knapp
+document.querySelector('.add-plant-btn')
+    .addEventListener('click', addPlantToList);
+
+function addPlantToList() {
+    //2. när knappen tryckts på skall vi ta värdet som ligger i vårt input-fält
+    let plantInput = document.getElementById('plant-input');
+    //vi kommer åt värdet i ett input fält genom att använda oss av '.value'
+    plantInput = plantInput.value;
+    //3. ta värdet och lägga in det i vår plantList
+    plantList.push(plantInput);
+    //tömmer input-fältet efter att vi tryckt på knappen
+    plantInput.value = "";
+    generatePlantListUI();
+}
