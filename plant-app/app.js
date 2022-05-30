e74c8367c15969e556dad0b40e106f2e1c714296
Maja Gedda <maja.gedda@zocom.se>
2022-05-30 11:12:46 +0200

    generate plants to ui via loop added

--- plant-app/app.js
+++ plant-app/app.js
@@ -1,3 +1,4 @@
+//vår initiala lista med plantor
 let plantList = [
     "Sunflower",
     "Coleus",
@@ -5,3 +6,25 @@ let plantList = [
     "Rose",
     "Daisy"
 ];
+
+//komma åt vår ul
+const plantListElement = document.getElementById('plant-list');
+
+function generatePlantListUI() {
+    //loopa över listan och för varje planta skall vi
+    //trycka in den i vår ul med id plant-list
+
+    for (let i = 0; i < plantList.length; i++) {
+        //sparar nuvarande element i loopen i variabeln 'plant'
+        let plant = plantList[i];
+        //skapa ett nytt element
+        let newElement = document.createElement('li');
+        //lägger in strängen som vi fått från listan genom vår loop i vårt nya element
+        newElement.innerText = plant;
+        console.log(newElement);
+        //nu vill vi lägga in plantan i vår ul
+        plantListElement.appendChild(newElement);
+    }
+};
+
+generatePlantListUI();
